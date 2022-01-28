aac9a5a95af0f66347337b015c2aebe9fabc61c9
Mahadhav1999 <mahadhavdhobale@gmail.com>
2022-01-28 23:35:58 +0530

    changes

--- db.js
+++ db.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://admin:admin@cluster0.asvkj.mongodb.net/zomato?retryWrites=true&w=majority")
+let URL = "mongodb+srv://admin:admin@cluster0.asvkj.mongodb.net/zomato?retryWrites=true&w=majority"
+mongoose.connect(URL, {useNewUrlParser:true},(err)=>{
+    if(err) {
+        console.log(err);
+    }else{
+        console.log("Successfully Connected to Database");
+    }
+});
+
+// var mongoose = require('mongoose');
+// mongoose.connect("mongodb+srv://admin:admin@cluster0.asvkj.mongodb.net/zomato?retryWrites=true&w=majority")
